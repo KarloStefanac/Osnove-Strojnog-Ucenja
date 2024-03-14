ecd9bc35b6af6258441af16858c800d024816af2
KarloStefanac <116030260+KarloStefanac@users.noreply.github.com>
2024-03-14 13:20:08 +0100

    Final commit

--- LV2/zatadak_3.py
+++ LV2/zatadak_3.py
@@ -7,13 +7,14 @@ img = img[:,:,0].copy()
 print(img.shape)
 print(img.dtype)
 plt.figure()
-plt.imshow(img, cmap="gray")
+plt.title("Brightened image")
 plt.imshow(img, alpha=0.5,cmap="gray")
 # plt.show()
 
 #b)
 height, width = img.shape
 plt.figure()
+plt.title("Second quarter of the image")
 plt.imshow(img[:, int(width*(1/4)):int(width*(1/2))], cmap="gray")
 # plt.show()
 
@@ -24,11 +25,13 @@ for i in range(height):
         rotated[j][width-i-1] = img[i][j]
 rotated = rotated[:, width-height:]
 plt.figure()
+plt.title("Image rotated 90° clockwise")
 plt.imshow(rotated,cmap="gray")
 # plt.show()
 
 #d)
 mirrored = np.fliplr(img)
 plt.figure()
+plt.title("Mirrored image")
 plt.imshow(mirrored,cmap="gray")
 plt.show()
