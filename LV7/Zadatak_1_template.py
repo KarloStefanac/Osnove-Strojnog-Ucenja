6feb29d7ec2e3327860e03a363daefa5b4bd9e7e
KarloStefanac <karlostef6@gmail.com>
2024-04-18 08:48:31 +0200

    Zadatak 1

--- LV7/Zadatak_1_template.py
+++ LV7/Zadatak_1_template.py
@@ -47,4 +47,50 @@ plt.scatter(X[:,0],X[:,1])
 plt.xlabel('$x_1$')
 plt.ylabel('$x_2$')
 plt.title('podatkovni primjeri')
+# plt.show()
+
+# flag 1=> 3 grupe 
+# flag 2=> 3 grupe
+# flag 3=> 4 grupe
+# flag 4=> 2 grupe?
+# flag 5=> 2 grupe?
+
+#r Pimijenite metodu K srednjih vrijednosti te ponovo prikažite primjere, ali svaki primjer
+# obojite ovisno o njegovoj pripadnosti pojedinoj grupi. Nekoliko puta pokrenite programski
+# kod. Mijenjate broj K. Što primjecujete?
+km = KMeans(n_clusters=3)
+km.fit(X)
+labels = km.predict(X)
+plt.figure()
+plt.scatter(X[:,0],X[:,1], c=labels)
+# plt.show()
+
+X2 = generate_data(500, 2)
+km = KMeans(n_clusters=3)
+km.fit(X2)
+labels = km.predict(X2)
+plt.figure()
+plt.scatter(X2[:,0],X2[:,1], c=labels)
+
+X3 = generate_data(500, 3)
+km = KMeans(n_clusters=4)
+km.fit(X3)
+labels = km.predict(X3)
+plt.figure()
+plt.scatter(X3[:,0],X3[:,1], c=labels)
+
+X4 = generate_data(500, 4)
+km = KMeans(n_clusters=4)
+km.fit(X4)
+labels = km.predict(X4)
+plt.figure()
+plt.scatter(X4[:,0],X4[:,1], c=labels)
+
+X5 = generate_data(500, 5)
+km = KMeans(n_clusters=4)
+km.fit(X5)
+labels = km.predict(X5)
+plt.figure()
+plt.scatter(X5[:,0],X5[:,1], c=labels)
+
 plt.show()
